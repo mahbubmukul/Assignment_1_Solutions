1f0547952f2921044e345b2b0013f05d6e2b34a9
mahbubmukul <mahbub@evaly.com.bd>
2021-12-13 12:39:43 +0600

    image

--- app/src/main/java/com/example/assignment_1solutions/MainActivity.java
+++ app/src/main/java/com/example/assignment_1solutions/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.assignment_1solutions;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_1);
+        setContentView(R.layout.layout_5);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
     }
 }
