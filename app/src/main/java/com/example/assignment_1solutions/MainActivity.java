b368f0c473608a95ccbb30195ff683e149732a39
mahbubmukul <mahbub@evaly.com.bd>
2021-12-13 12:20:15 +0600

    Initial commit

---
+++ app/src/main/java/com/example/assignment_1solutions/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.assignment_1solutions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_1);
+    }
+}
